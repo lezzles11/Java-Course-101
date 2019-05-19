beaeabc5f6323c1b80bb2fa18a166ba7dcb8f83d
lezzles11 <lesleyc@bu.edu>
2019-05-19 13:10:24 -0400

    added tmc projects

---
+++ TMCProjects/mooc-2013-OOProgrammingWithJava-PART1/week1-022.Password/src/Password.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Password {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        String password = "carrot"; // Use carrot as password when running tests.
+
+        System.out.println("Type the password: ");
+        String enter = reader.nextLine();
+        if (!(password.equals(enter))) {
+            System.out.println("Wrong!");
+        } else {
+            System.out.println("The secret is: jryy!")
+        }
+    }
+}
